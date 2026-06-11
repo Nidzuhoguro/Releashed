185d1841642a02bbc43f000707a6b8d66dcb291f
Nidzuhoguro <quietxpupil@proton.me>
2026-06-11 15:16:22 +0400

    rewrote code to work with the new HashMap registry

--- src/main/java/me/nidzuhoguro/releashed/Handler.java
+++ src/main/java/me/nidzuhoguro/releashed/Handler.java
@@ -1,5 +1,6 @@
 package me.nidzuhoguro.releashed;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -18,107 +19,102 @@ import org.bukkit.event.player.*;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
 
 public class Handler implements Listener {
 
-    private static final Releashed releashed = Releashed.getPlugin(Releashed.class);
+    private final Releashed releashed = Releashed.PLUGIN;
 
     @EventHandler
     public void onPlayerDisconnect(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        ArrayList<Pair> pairs = Pair.getAllPairs(player);
-
-        for (Pair pair : pairs) {
-            if (pair.submissive.equals(player)) player.damage(20.0); // Deals great damage to the sub if they disconnect.
-            pair.dominant.getInventory().addItem(new ItemStack(Material.LEAD));
-            pair.unleash(false);
-        }
+        releashed.getSubPairs(player.getUniqueId()).forEach(p -> player.damage(20.0));
     }
 
     @EventHandler
     public void onPlayerDie(PlayerDeathEvent event) {
-        ArrayList<Pair> pairs = Pair.getAllPairs(event.getEntity());
-
-        for (Pair pair : pairs) {
-            if (pair.isAttached() && event.getEntity() != pair.submissive) continue; // If the dominant left the sub tied to a fence and died, the sub stays tied.
-            pair.unleash(false);
-            pair.dominant.getInventory().addItem(new ItemStack(Material.LEAD));
+        UUID player = event.getEntity().getUniqueId();
+        for (Pair pair : releashed.getAllPairs(event.getEntity().getUniqueId())) {
+            if (player.equals(pair.dominantID) && !pair.isAttached()) pair.invalidate();
         }
     }
 
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
-        ArrayList<Pair> pairs = Pair.getAllPairs(event.getPlayer());
+        UUID player = event.getPlayer().getUniqueId();
+        float distance = event.getTo() == null ? 100.0f : (float) event.getFrom().distance(event.getTo());
 
-        for (Pair pair : pairs) {
-            if (pair.isAttached() && event.getPlayer() != pair.submissive) continue; // If the dominant left the sub tied to a fence and teleported away, the sub stays tied.
-            pair.unleash(false);
-            pair.dominant.getInventory().addItem(new ItemStack(Material.LEAD));
+        for (Pair pair : releashed.getAllPairs(player)) {
+            if (!pair.isAttached() && pair.isDominant(player)) {
+                pair.invalidate();
+                continue;
+            }
+
+            if (distance > 40.0f) pair.invalidate();
         }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onUnleash(EntityUnleashEvent event) {
-        ArrayList<Pair> discard = new ArrayList<>();
-        for (Pair pair : releashed.pairs) {
+        for (Pair pair : releashed.pairs.values().stream().flatMap(Collection::stream).toList()) {
             if (pair.leashMount == null) continue; // Prevent a potential NullPointerException.
             if (pair.leashMount.equals(event.getEntity())) {
-                if (pair.unleash(true)) discard.add(pair);
+                pair.invalidate();
             }
         }
-
-        releashed.pairs.removeAll(discard); // Fixed ConcurrentModificationException.
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerAttack(EntityDamageByEntityEvent event) {
         if (!(event.getDamager() instanceof Player)) return;
+        if (!(event.getEntity() instanceof Player)) return;
 
-        // Subs take damage equal to that they dealt to their dominants
-        if (event.getEntity() instanceof Player) {
-            ArrayList<Pair> pairs = Pair.getAllPairs((Player) event.getDamager());
+        List<Pair> subPairs = releashed.getSubPairs(event.getDamager().getUniqueId());
 
-            for (Pair pair : pairs) {
-                if (pair.dominant.equals(event.getEntity())) ((Player) event.getDamager()).damage(event.getDamage());
-            }
+        for (Pair pair : subPairs) {
+            if (pair.dominantID == event.getEntity().getUniqueId())
+                ((Player) event.getDamager()).damage(event.getDamage());
         }
 
         if (event.getEntity().getType().equals(EntityType.LEASH_KNOT)) {
-            if (!Pair.getSubmissivePairs((Player) event.getDamager()).isEmpty()) event.setCancelled(true); // If the sub attacks their leash knot, cancel the event.
+            event.setCancelled(true);
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST) // EXPERIMENTAL
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void attachToFence(PlayerInteractEvent event) {
-        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock() != null) {
-            if (event.getClickedBlock().getType().name().endsWith("_FENCE")) {
-                if (event.getHand() == EquipmentSlot.OFF_HAND || Pair.getAllPairs(event.getPlayer()).isEmpty()) return;
-                Location location = event.getClickedBlock().getLocation().add(0.0, 0.5, 0.0);
-                Entity knot = event.getPlayer().getWorld().spawnEntity(location, EntityType.LEASH_KNOT);
+        if (event.getHand() == EquipmentSlot.OFF_HAND) return;
+        List<Pair> domPairs = releashed.getDomPairs(event.getPlayer().getUniqueId());
+        if (!(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) || event.getClickedBlock() == null) return;
+        if (!(event.getClickedBlock().getType().name().endsWith("_FENCE"))) return;
 
-                ArrayList<Pair> pairs = Pair.getAllPairs(event.getPlayer());
+        Location location = event.getClickedBlock().getLocation().add(0.0, 0.5, 0.0);
+        Entity knot = event.getPlayer().getWorld().spawnEntity(location, EntityType.LEASH_KNOT);
 
-                for (Pair pair : pairs) {
-                    pair.attachToBlock(location, knot, event.getClickedBlock());
-                }
-            }
+        for (Pair pair : domPairs) {
+            pair.attachToBlock(location, knot, event.getClickedBlock());
         }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onHangingDamage(HangingBreakByEntityEvent event) {
         if (!(event.getRemover() instanceof Player)) return; // Fixed potential ClassCastException.
-        if (event.getEntity().getType().equals(EntityType.LEASH_KNOT) && !Pair.getSubmissivePairs((Player) event.getRemover()).isEmpty()) {
-            event.setCancelled(true);
-        }
+        if (!event.getEntity().getType().equals(EntityType.LEASH_KNOT)) return;
+
+        UUID player = event.getRemover().getUniqueId();
+        List<Pair> subPairs = releashed.getSubPairs(player);
+
+        if (!subPairs.isEmpty()) event.setCancelled(true);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onBlockBreak(BlockBreakEvent event) {
-        if (event.getBlock().getType().name().endsWith("_FENCE") && !Pair.getSubmissivePairs(event.getPlayer()).isEmpty()) { // If a sub tries to break a fence
-            ArrayList<Pair> pairs = Pair.getAllPairs(event.getPlayer());
-            for (Pair pair : pairs) {
+        UUID player = event.getPlayer().getUniqueId();
+        List<Pair> subPairs = releashed.getSubPairs(player);
+        if (event.getBlock().getType().name().endsWith("_FENCE") && !subPairs.isEmpty()) { // If a sub tries to break a fence
+            for (Pair pair : subPairs) {
                 if (pair.getFence().equals(event.getBlock())) event.setCancelled(true);
             }
         }
@@ -126,61 +122,42 @@ public class Handler implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerInteract(PlayerInteractAtEntityEvent event) {
+        UUID actorID = event.getPlayer().getUniqueId();
 
-        Player player = event.getPlayer();
-
-        if (event.getHand() == EquipmentSlot.OFF_HAND) return;
         if (event.getRightClicked().getType() == EntityType.LEASH_KNOT) {
-
-            if (!Pair.getSubmissivePairs(event.getPlayer()).isEmpty()) { // Subs cannot untie the leash knots
-                event.setCancelled(true);
-                return;
-            }
-
-            ArrayList<Pair> pairs = Pair.getAllPairs(event.getPlayer());
-
-            for (Pair pair : pairs) {
-                if (event.getRightClicked() == pair.leashMount) pair.detachFromBlock();
+            for (Pair pair : releashed.getDomPairs(actorID)) {
+                pair.detachFromBlock();
             }
             return;
         }
 
-        if (!(event.getRightClicked() instanceof Player)) return;
+        Entity clicked = event.getRightClicked();
 
-        Entity target = event.getRightClicked();
+        if (!(clicked instanceof Player)) return;
 
-        ItemStack domItem = player.getInventory().getItemInMainHand();
+        UUID targetID = clicked.getUniqueId();
+        UUID actorDominant = releashed.getDominant(actorID);
 
-        ArrayList<Pair> domPairs = Pair.getAllPairs(player);
-        ArrayList<Pair> subPairs = Pair.getAllPairs((Player) target);
-
-        for (Pair pair : domPairs) {
-            if (pair.submissive.equals(target)) {
-                pair.unleash(false);
-                player.getInventory().addItem(new ItemStack(Material.LEAD));
-                return;
-            }
+        if (targetID.equals(actorDominant)) {
+            Player actor = Bukkit.getPlayer(actorID);
+            if (actor == null) return;
+            actor.sendMessage("You cannot leash your dominant!");
+            return;
         }
 
-        if (domItem.getType() != Material.LEAD) return;
+        List<Pair> actorDomPairs = releashed.getDomPairs(actorID).stream().filter(p -> p.submissiveID.equals(targetID)).toList();
 
-        for (Pair pair : subPairs) {
-            if (!pair.isDominant((Player) target)) {
-                player.sendMessage("This player is already leashed");
-                return;
-            }
+        if (actorDomPairs.isEmpty()) {
+            Player dominant = Bukkit.getPlayer(actorID);
+            if (dominant == null) return;
+            ItemStack domItem = dominant.getInventory().getItemInMainHand();
+            if (domItem.getType() != Material.LEAD) return;
+            domItem.setAmount(domItem.getAmount() - 1);
+            dominant.getInventory().setItemInMainHand(domItem);
+            releashed.pair(actorID, targetID);
+            return;
         }
 
-        for (Pair pair : domPairs) {
-            if (pair.dominant.equals(target)) {
-                player.sendMessage("You cannot leash your dominant~");
-                return;
-            }
-        }
-
-        Pair pair = new Pair(player, (Player) target);
-        domItem.setAmount(domItem.getAmount() - 1);
-        player.getInventory().setItemInMainHand(domItem);
-        releashed.pairs.add(pair);
+        actorDomPairs.forEach(releashed::unpair);
     }
 }
